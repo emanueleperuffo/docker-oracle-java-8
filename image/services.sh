e956bfdcc26570035c57bb7a711461d3cdd6d778
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-19 14:29:39 +0200

    fixing

--- image/services.sh
+++ image/services.sh
@@ -6,5 +6,8 @@ set -x
 # Accepting Oracle JDK License
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 
-## Installing Postgres
-$minimal_apt_get_install postgresql-${PG_MAJOR} postgresql-contrib-${PG_MAJOR} postgis-${POSTGIS_VERSION}
+## Installing Java 8 and setting it as the default
+$minimal_apt_get_install oracle-java8-installer oracle-java8-set-default
+
+## Installing sbt
+$minimal_apt_get_install --force-yes sbt
